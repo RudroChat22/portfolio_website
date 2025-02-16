9108d343cda0dbda2a1d87a847c25db6a18a4cee
Rudro Chatterji <herodrone43@gmail.com>
2025-02-16 13:46:20 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // Import path module
+
+export default defineConfig({
+  plugins: [react()],
+  base:"portfolio_website",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Define alias for "src"
+    },
+  },
+});
